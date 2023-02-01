20bddd1a62d8045ff5af338a5e9d79a43356f1f1
Andrés Mateo Gabín <andres.mgabin@upm.es>
2023-02-01 14:00:09 -0500

    Use indent-blankline to print the indent lines

--- lua/plugins/configs/mini.lua
+++ lua/plugins/configs/mini.lua
@@ -1,13 +1,13 @@
 -- Mini.pairs --
 local ppairs, autopairs = pcall(require, 'mini.pairs')
 if ppairs then
-    autopairs.setup{}
+    autopairs.setup {}
 end
 
 -- Mini.comment --
 local pcomment, comment = pcall(require, 'mini.comment')
 if pcomment then
-    comment.setup{}
+    comment.setup {}
 end
 
 -- Mini.indentscope --
@@ -17,37 +17,38 @@ if pindent then
         draw = {
             animation = indent.gen_animation.none()
         },
+        symbol = '',
     }
 end
 
 -- Mini.jump --
 local pjump, jump = pcall(require, 'mini.jump')
 if pjump then
-    jump.setup{}
+    jump.setup {}
 end
 
 -- Mini.move --
 local pmove, move = pcall(require, 'mini.move')
 if pmove then
-    move.setup{}
+    move.setup {}
 end
 
 -- Mini.surround --
 local psurround, surround = pcall(require, 'mini.surround')
 if psurround then
-    surround.setup{}
+    surround.setup {}
 end
 
 -- Mini.tabline --
 local ptabline, tabline = pcall(require, 'mini.tabline')
 if ptabline then
-    tabline.setup{}
+    tabline.setup {}
 end
 
 -- Mini.trailspace --
 local ptrailspace, trailspace = pcall(require, 'mini.trailspace')
 if ptrailspace then
-    trailspace.setup{}
+    trailspace.setup {}
     vim.api.nvim_create_autocmd(
     { 'BufWritePost' },
     {
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -138,6 +138,16 @@ require('packer').startup {
             end,
         }
 
+        -- Indenting lines --
+        use {
+            'lukas-reineke/indent-blankline.nvim',
+            config = function()
+                require('indent_blankline').setup {
+                    show_current_context = true,
+                }
+            end,
+        }
+
         -- Scrollbar --
         use {
             'petertriho/nvim-scrollbar',
